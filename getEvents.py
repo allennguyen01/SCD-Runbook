919f59ec25985c5674db7866ee6ebca96c0265f7
Allen Nguyen <anhn321@gmail.com>
2022-09-21 13:57:06 -0700

    Get only FileTrigger Events and not all Event objects

--- getEvents.py
+++ getEvents.py
@@ -8,25 +8,29 @@ Events = root.iter('Event')
 
 eventsDict = {}
 eventsList = []
-eventColumns = ['ID', 'Trigger Label', 'Enabled Flag', 'PID Name', 'Description']
+eventColumns = ['ID', 'Trigger Label', 'Enabled Flag', 'Triggered Plan', 'Description']
 
 # Iterate through every Event element 
 for event in Events:
     typeid = event.get('typeid')
-    if typeid == 'Event' or typeid == 'DateTimeEvent':
-        # Store Events in dictionary with {ID : [name, enabledFlag, PIDName]}
-        ID = event.find('ID').find('ID').text
+    if typeid == 'Event':
+        # Store FileTriggers in dictionary with {ID : [Name, EnabledFlag, TriggeredPlanName, FileDescription]}
         label = event.find('ID').find('Label').text
-        # if 'FileTrigger' not in label:
-        #     continue
+        if 'FileTrigger' not in label:
+            continue
+
+        ID = event.find('ID').find('ID').text
+        
         enabledFlag = event.find('Enabled').text
         PIDName = event.find('PID').find('Name').text
+
         description = ''
         for var in event.find('Properties').findall('Variable'):
             varName = var.find('Name').text
             if varName != 'Filter':
                 continue
             description = var.find('Value').text
+
         eventsDict.update({ID : [label, enabledFlag, PIDName, description]})
 
-createCSV(eventsList, eventsDict, eventColumns, 'outputCSV\Events_Test_1.csv')
+createCSV(eventsList, eventsDict, eventColumns, 'outputCSV\Events.csv')
