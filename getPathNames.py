12548e73f75922fe00b0765df90fdca2282db2a1
Allen Nguyen <anhn321@gmail.com>
2022-09-21 13:00:28 -0700

    Changed file name from getPID to getPathNames

--- getPathNames.py
+++ getPathNames.py
@@ -17,15 +17,13 @@ for obj in Objects:
         ID = obj.find('ID').text
         name = obj.find('Name').text
         PID = obj.find('PID').find('ID').text
-        enabled = obj.find('Enabled').text
-        pidDict.update({ID: [name, PID, typeid, enabled]})
+        pidDict.update({ID: [name, PID, typeid]})
 
 # Create path name
 for id in pidDict:
     name = pidDict[id][0]
     pid = pidDict[id][1]
     typeid = pidDict[id][2]
-    enabled = pidDict[id][3]
     pathName = ''
     tempPID = pid
     tempName = name
@@ -35,12 +33,12 @@ for id in pidDict:
             break
         tempName = pidDict[tempPID][0]
         tempPID = pidDict[tempPID][1]
-    pidDict.update({id : [name, pid, typeid, enabled, pathName]})
+    pidDict.update({id : [name, pid, typeid, pathName]})
 
 # trn_fit_id = '15101196'
 # print(pidDict[trn_fit_id][0], pidDict[trn_fit_id][(len(pidDict[trn_fit_id])-1)])
 
 # Create a list of all Job Plans as dictionaries
-pidColumns = ['ID', 'Name', 'PID', 'Type ID', 'Enabled', 'Path Name']
+pidColumns = ['ID', 'Name', 'PID', 'Type ID', 'Path Name']
 
 createCSV(pidList, pidDict, pidColumns, 'outputCSV\Path_Names_test.csv')
