13e728ba379439ad132926522a1f59db789dbfeb
Allen Nguyen <anhn321@gmail.com>
2022-09-27 11:54:02 -0700

    Gets the child->parent relationship, but is missing first job and top-level plan

--- getExecutionOrder.py
+++ getExecutionOrder.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
+from getPathNames import pidDict 
 
 tree = ET.parse('XMLs\SCD_DWH_LOAD.xml')
 root = tree.getroot()
@@ -10,17 +11,43 @@ eventsDict = {}
 eventsList = []
 
 # Iterate through every Event element 
+# for event in Events:
+#     typeid = event.get('typeid')
+#     if typeid == 'Event':
+#         label = event.find('ID').find('Label').text
+#         if 'FileTrigger' in label:
+#             continue
+#         ID = event.find('PID').find('ID').text
+#         pidName = event.find('PID').find('Name').text
+#         nextActionID = event.find('Actions').find('EventAction').find('CommandArgument').find('ID')
+#         nextActionID = nextActionID.text if nextActionID is not None else ''
+#         eventsList.append([ID, label, pidName, nextActionID])
+
 for event in Events:
     typeid = event.get('typeid')
     if typeid == 'Event':
+        ID = event.find('Actions').find('EventAction').find('CommandArgument').find('ID')
+        ID = ID.text if ID is not None else ''
+        name = pidDict[ID][0]
+
         label = event.find('ID').find('Label').text
         if 'FileTrigger' in label:
             continue
-        ID = event.find('PID').find('ID').text
-        pidName = event.find('PID').find('Name').text
-        nextActionID = event.find('Actions').find('EventAction').find('CommandArgument').find('ID')
-        nextActionID = nextActionID.text if nextActionID is not None else ''
-        eventsList.append([ID, label, pidName, nextActionID])
+        parentID = event.find('PID').find('ID').text
+        parentName = event.find('PID').find('Name').text
+        
+        eventsList.append([ID, name, parentName, parentID])
+        eventsDict.update({ID : [name, parentName, parentID]})
 
-actionsDF = pd.DataFrame(eventsList, columns=['ID', 'Action Label', 'Parent Name', 'Next Action ID'])
-actionsDF.to_csv('outputCSV\ExecutionOrder.csv', index=False)
+for id in eventsDict:
+    currParentID = eventsDict[id][2]
+    if currParentID not in eventsDict:
+        ID = currParentID
+        name = pidDict[ID][0]
+        parentID = pidDict[ID][1]
+        parentPathName = pidDict[ID][4]
+        eventsList.append(ID, name, parentPathName, parentID)
+
+
+eoDF = pd.DataFrame(eventsList, columns=['ID', 'Name', 'Parent Name', 'Parent ID'])
+eoDF.to_csv('outputCSV\ExecutionOrder_SCD_DWH_LOAD_1.csv', index=False)
