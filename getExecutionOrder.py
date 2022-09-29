97b1843b22731228da4cd7925247f221bdc6e40b
Allen Nguyen <anhn321@gmail.com>
2022-09-29 16:34:34 -0700

    Get all events required to create tree view in SQL

--- getExecutionOrder.py
+++ getExecutionOrder.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 from getPathNames import pathNameDF 
 
-tree = ET.parse('XMLs\SCD_DWH_LOAD.xml')
+tree = ET.parse('XMLs\BREAKFIX_20220913.xml')
 root = tree.getroot()
 
 Events = root.iter('Event')
@@ -10,33 +10,27 @@ Events = root.iter('Event')
 eventsDict = {}
 eventsList = []
 
-# Iterate through every Event element 
-# for event in Events:
-#     typeid = event.get('typeid')
-#     if typeid == 'Event':
-#         label = event.find('ID').find('Label').text
-#         if 'FileTrigger' in label:
-#             continue
-#         ID = event.find('PID').find('ID').text
-#         pidName = event.find('PID').find('Name').text
-#         nextActionID = event.find('Actions').find('EventAction').find('CommandArgument').find('ID')
-#         nextActionID = nextActionID.text if nextActionID is not None else ''
-#         eventsList.append([ID, label, pidName, nextActionID])
+triggerDictionary = {'2' : 'Alert Trigger: Job/Plan Completed', '4' : 'Failed Completion Trigger', '5' : 'Success Completion Trigger'}
 
 for event in Events:
     typeid = event.get('typeid')
     if typeid == 'Event':
         ID = event.find('Actions').find('EventAction').find('CommandArgument').find('ID')
         ID = ID.text if ID is not None else ''
+        if ID == '':
+            continue
         name = pathNameDF.loc[ID]['Name']
 
         label = event.find('ID').find('Label').text
         if 'FileTrigger' in label:
             continue
+        # eventID = event.find('EventID').text
+        v4Tag = event.find('V4Tag').text
+        label = triggerDictionary[v4Tag] if v4Tag in triggerDictionary else ''
         parentID = event.find('PID').find('ID').text
         parentName = event.find('PID').find('Name').text
         
-        eventsList.append([ID, name, parentName, parentID])
+        eventsList.append([ID, name, parentName, parentID, label])
         eventsDict.update({ID : [name, parentName, parentID]})
 
 for id in eventsDict:
@@ -44,21 +38,26 @@ for id in eventsDict:
     if currParentID not in eventsDict:
         # Add first job that executes
         ID = currParentID
-        currObj = pathNameDF.loc[ID]
+        try:
+            currObj = pathNameDF.loc[ID]
+        except:
+            continue
         name = currObj['Name']
         parentName = currObj['Parent Name']
         parentID = currObj['Parent ID']
-        eventsList.append([ID, name, parentName, parentID])
+        label = 'First Batch Job Group'
+        eventsList.append([ID, name, parentName, parentID, label])
 
         # Add top level plan
         ID = parentID
         currObj = pathNameDF.loc[ID]
         name = currObj['Path Name']
         parentName = currObj['Parent Name']
-        parentID = None
-        eventsList.append([ID, name, parentName, parentID])
+        parentID = currObj['Parent ID']
+        label = 'Top-level Plan'
+        eventsList.append([ID, name, parentName, parentID, label])
 
-eoDF = pd.DataFrame(eventsList, columns=['ID', 'Name', 'Parent Name', 'Parent ID'])
+eoDF = pd.DataFrame(eventsList, columns=['ID', 'Name', 'Parent Name', 'Parent ID', 'Label'])
 eoDF = eoDF.set_index('ID')
 
-eoDF.to_csv('outputCSV\ExecutionOrder_SCD_DWH_LOAD.csv')
+eoDF.to_csv('outputCSV\ExecutionOrder_test_1.csv')
