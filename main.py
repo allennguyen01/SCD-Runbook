2fb1362d7e9d997cd108fda6c1be174a007f4330
Allen Nguyen <anhn321@gmail.com>
2022-10-12 16:27:38 -0700

    Create csv files all at once

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from getJobPlans import getJobPlans
+from getScheduledPlans import getScheduledPlans
+from getVariables import getVariables
+from getPathNames import getPathNames
+from getExecutionOrder import getExecutionOrder
+from getSchedules import getSchedules
+
 
 if __name__ == '__main__':
+    getSchedules('XMLs\PROD_20221004.xml', 'outputCSV\Schedules_PROD.csv')
     
-    getJobPlans('XMLs\PROD_20221004.xml', 'outputCSV\Schedules_PROD.csv', 'outputCSV\JobPlans_PROD.csv')
-    # getVariables('XMLs\PROD_20221004.xml', 'outputCSV\Variables_PROD.csv')
+    # depends on Schedules csv file
+    getScheduledPlans('XMLs\PROD_20221004.xml', 'outputCSV\Schedules_PROD.csv', 'outputCSV\JobPlans_PROD.csv')
 
-    # getPathNames('XMLs\PROD_20221004.xml', 'outputCSV\PathNames_PROD.csv')
+    getVariables('XMLs\PROD_20221004.xml', 'outputCSV\Variables_PROD.csv')
 
-    # # getExecutionOrder depends on PathNames csv
-    # getExecutionOrder('XMLs\PROD_20221004.xml', 'outputCSV\PathNames_PROD.csv', 'outputCSV\ExecutionOrder_PROD.csv')
+    getPathNames('XMLs\PROD_20221004.xml', 'outputCSV\PathNames_PROD.csv')
+
+    # getExecutionOrder depends on PathNames csv file
+    getExecutionOrder('XMLs\PROD_20221004.xml', 'outputCSV\PathNames_PROD.csv', 'outputCSV\ExecutionOrder_PROD.csv')
     
