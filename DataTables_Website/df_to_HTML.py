aaacbdad1908fb2633150d4d71de1eb55f42f9d5
Allen Nguyen <anhn321@gmail.com>
2022-10-18 10:36:21 -0700

    Add comments

--- DataTables_Website/df_to_HTML.py
+++ DataTables_Website/df_to_HTML.py
@@ -1,23 +1,30 @@
 from sql_to_dataframe import sql_to_dataframe
 
+# Convert a Dataframe to a HTML table and return the string
 def df_to_HTMLTable(df):
     columnNames = df.columns.tolist()
     tabTab = f'\t\t'
 
+    # Form the thead tag with column field names
     tableHead = f"{tabTab}<thead>\n\t{tabTab}<tr style=\"text-align: right;\">\n\t{tabTab}<th>INDEX</th><th>{'</th><th>'.join(columnNames)}</th></tr>\n{tabTab}</thead>"
 
+    # Form the tbody tag with data rows
     tableBody = f"\n{tabTab}<tbody>\n"
     for i in range(len(df)):
         plan = df.iloc[i]
         data = [plan[colName] for colName in columnNames]
         data = ["" if value is None else value for value in data]
         name = data[0]
+
+        # Add hyperlink to name field
         data[0] = f"<a href=\"/{name}\">{name}</a>"
 
         tableBody += f"\t{tabTab}<tr>\n\t\t{tabTab}<th>{i}</th>\n\t\t{tabTab}<td>{'</td><td>'.join(data)}</td>\n\t{tabTab}</tr>\n"
     tableBody += f"{tabTab}</tbody>\n"
 
+    # Wrap table tag around thead and tbody
     table = f"<table border=\"1\" class=\"dataframe table table-hover table-bordered table-striped\" id=\"table_id\">\n{tableHead}{tableBody}{tabTab}</table>"
+    
     return table
 
 def main():
