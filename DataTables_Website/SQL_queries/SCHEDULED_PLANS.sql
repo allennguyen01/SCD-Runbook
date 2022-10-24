b31c3f24a6707d36073d4a82667d9f735afb7a97
Allen Nguyen <anhn321@gmail.com>
2022-10-24 10:24:57 -0700

    Change query to select all rows

--- DataTables_Website/SQL_queries/SCHEDULED_PLANS.sql
+++ DataTables_Website/SQL_queries/SCHEDULED_PLANS.sql
@@ -1,5 +1,4 @@
-SELECT TOP (10)
-        [NAME] AS 'Name'
+SELECT [NAME] AS 'Name'
       ,[PATH_NAME] AS 'Path Name'
       ,[ENTIRE_SCHEDULES] AS 'Scheduled Times'
   FROM [SCDRunbook].[dbo].[SCHEDULED_PLANS_LIST]
