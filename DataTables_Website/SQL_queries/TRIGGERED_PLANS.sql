ce9447460ad233f4dc44b418c55757c6a53d7033
Allen Nguyen <anhn321@gmail.com>
2022-10-24 12:08:24 -0700

    Add SQL query to select all TRIGGERED_PLANS_LIST

---
+++ DataTables_Website/SQL_queries/TRIGGERED_PLANS.sql
@@ -0,0 +1,6 @@
+SELECT [NAME] as 'Name'
+      ,[PATH_NAME] as 'Path Name'
+      ,[STATE] as 'State'
+      ,[DESCRIPTION] as 'Description'
+      ,[ID] as 'ID'
+  FROM [SCDRunbook].[dbo].[TRIGGERED_PLANS_LIST]
