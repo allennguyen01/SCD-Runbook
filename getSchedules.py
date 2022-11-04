dd2d4d54c2df95696b9d98ef7963087bff6d0f69
Allen Nguyen <anhn321@gmail.com>
2022-11-04 15:59:27 -0700

    Retrieve all the Time Specifications with text matching

--- getSchedules.py
+++ getSchedules.py
@@ -1,16 +1,15 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
-from datetime import *
+from datetime import time
 
 # Gets the Time Specification from ActiveBatch XML as HH:MM:SS format
 def getTime(obj):
+    name = obj.findtext('Name')
     try:
-        timeText = obj.findall('./Properties/Variable')[1].findall('./Value/Variable/Value/Variable')[2].findtext('Value')
-    except IndexError:
-        print(IndexError, 'Scheduled Time Not Found', obj.findtext('Name'))
+        timeText = obj.find(".//*[.='ExactTimes']../Value/Variable/Value/.//*[.='Value']../Value").text
+    except AttributeError:
+        print(f'{AttributeError}, Time Specification not provided for {name}')
         return
-
-    # timeText = obj.findtext('[[.="ExactTimes"]="Value"]/../Value')
 
     # Get time in HH:MM:SS format as time object 
     timeHMS = timeText.split('T')[1].split(':')
@@ -20,7 +19,6 @@ def getTime(obj):
     return t
 
 # TODO: Find ActiveBatch Day Specifications and add them to scheduleDF (WEEKDAY, EOM, etc.)
-# BUG: Some Time Specifications are not being added (e.g. SCD_DAILY_1am, SCD_EOM_1145pm)
 def getSchedules(xmlFile, outputCSVFile):
     tree = ET.parse(xmlFile)
     root = tree.getroot()
@@ -35,18 +33,14 @@ def getSchedules(xmlFile, outputCSVFile):
             name = obj.findtext('Name')
             if name == '$Schedule':
                 continue
-            # timeText = obj.findtext('[[.="ExactTimes"]="Value"]/../Value')
-            # timeText = obj.findtext('//Name[.="ExactTimes"]')
-            # timeText = obj.findtext("ID[.='15104839']")
-            # print(ID, name, timeText)
-
             schTime = getTime(obj)
             scheduleList.append([ID, name, schTime])
 
+    # Output a CSV file from schedulesList using Pandas
     scheduleColumns = ['ID', 'SCHEDULE_NAME', 'TIME']
     scheduleDF = pd.DataFrame(scheduleList, columns=scheduleColumns)
     scheduleDF = scheduleDF.set_index('ID')
     scheduleDF.to_csv(outputCSVFile)
 
 if __name__ == "__main__":
-    getSchedules('XMLs\PROD_20221004.xml','outputCSV/Schedules_prod_1.csv')
+    getSchedules('XMLs\PROD_20221004.xml','outputCSV/Schedules_PROD.csv')
