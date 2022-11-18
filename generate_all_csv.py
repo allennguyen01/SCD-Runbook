fdfac132c46467eb16d4a8c92a190a0dad834996
Allen Nguyen <anhn321@gmail.com>
2022-11-18 13:47:24 -0800

    Add file error handling and success outputs

--- generate_all_csv.py
+++ generate_all_csv.py
@@ -5,16 +5,38 @@ from getExecutionOrder import getExecutionOrder
 from getSchedules import getSchedules
 from getFileTriggers import getFileTriggers
 from getBatchJobGroups import getBatchJobGroups
-
+import os
 
 if __name__ == '__main__':
-    xmlFile = input("What is the path of the XML file from ActiveBatch?")
+    while True:
+        try:
+            xmlFile = input("What is the path of the XML file from ActiveBatch? ")
+            f = open(xmlFile)
+            break
+        except FileNotFoundError:
+            print("FileNotFoundError. XML file does not exists, try again.")
+            
+    try:
+        os.mkdir("csv")
+    except FileExistsError:
+        print("csv folder already exists.")
 
     getSchedules(xmlFile, 'csv\Schedules.csv')
+    print("Schedules.csv generated sucessfully!")
+
     getPathNames(xmlFile, 'csv\PathNames.csv')
+    print("PathNames.csv generated sucessfully!")
+
     getVariables(xmlFile, 'csv\Variables.csv')
+    print("Variables.csv generated sucessfully!")
+
     getFileTriggers(xmlFile, 'csv\FileTriggers.csv')
-    getScheduledPlans(xmlFile, 'csv\Schedules.csv', 'csv\ScheduledJobs.csv')
+    print("FileTriggers.csv generated sucessfully!")
+
+    getScheduledPlans(xmlFile, 'csv\Schedules.csv', 'csv\ScheduledPlans.csv')
+    print("ScheduledPlans.csv generated sucessfully!")
+
     getBatchJobGroups(xmlFile, 'csv\PathNames.csv', 'csv\BatchJobGroups.csv')
+    print("BatchJobGroups.csv generated sucessfully!")
     
-    print("All csv file generated successfully!")
+    print("All csv files generated successfully in csv folder.")
